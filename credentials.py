2b4b96ad98aa56c2af3a347ae83103433e457ec3
Simon <simon.lindgren@umu.se>
2020-03-22 22:34:42 +0100

    Empty credentials

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = "uq7vzZA7n1wKavDcB07ggTTtu"
-consumer_secret = "MAgFI3Pf11kFN2VU52fSZQfNV6ZfVmKfqpArkYdsRQlSeGZb3T"
-access_secret = "DWdGbrMrZOegpDOfJHSBCApECk1lwzVCjIsQFuvSkIdJm"
-access_token = "223156951-NnWx0J0qdFxFmATXDWctPV92RV3L7hNJhDPANcav"
+consumer_key = ""
+consumer_secret = ""
+access_secret = ""
+access_token = ""
